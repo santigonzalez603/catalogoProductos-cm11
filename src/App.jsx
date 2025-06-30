c3d3093151cc7b6122752aa889d02c78e2fcd175
Santiago <concursos.utn.frt@gmail.com>
2025-06-30 20:36:03 -0300

    creamos las rutas del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, BrowserRouter, Routes } from "react-router";
 import Administrador from "./components/pages/Administrador";
 import DetalleProducto from "./components/pages/DetalleProducto";
 import Error404 from "./components/pages/Error404";
@@ -7,18 +8,24 @@ import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import Footer from "./components/shared/Footer";
 import Menu from "./components/shared/Menu";
 
+
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Menu></Menu>
       <main>
-        {/* <Error404></Error404> */}
-        {/* <Inicio></Inicio> */}
-        {/* <Administrador></Administrador> */}
-        {/* <DetalleProducto></DetalleProducto> */}
-        <FormularioProducto></FormularioProducto>
+        <Routes>
+          <Route path="/" element={<Inicio/>}></Route>
+          <Route path="/detalle" element={<DetalleProducto></DetalleProducto>}></Route>
+          <Route path="/administrador" element={<Administrador></Administrador>}></Route>
+          <Route path="/administrador/crear" element={<FormularioProducto></FormularioProducto>}></Route>
+          <Route path="/administrador/editar" element={<FormularioProducto></FormularioProducto>}></Route>
+          <Route path="*" element={<Error404></Error404>}></Route>
+        </Routes>
       </main>
       <Footer></Footer>
+      </BrowserRouter>
     </>
   );
 }
